7e3f60d67475b2e0a140e63ce7e689306e87e95c
LEIGH VARLEY <LEIGHVARLEY@GMAIL.COM>
2015-07-31 01:13:14 -0400

    saving changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,10 +19,13 @@ skip_before_action :authenticate_user!, only: [:index, :show]
   def create
     @post = Post.create!(post_params)
     redirect_to post_path(@post)
-    # if @post.save
-    #   redirect_to "/posts/#{@post.id}"
-    # else
-    #   redirect_to "/posts/new" end
+     #if @post.save
+       #redirect_to "/posts/#{@post.id}"
+       #redirect_to @article
+     #else
+       #redirect_to "/posts/new"
+       #render 'new'
+     #end
   end
 
   #show
@@ -50,13 +53,15 @@ skip_before_action :authenticate_user!, only: [:index, :show]
       redirect_to post_path
     end
 
-    #private method to allow us to pull info from the form
-    #require method ensure that a specific parameter is present
+    #Strong parameters - ActionController parameters cannot be used in Active Model mass assignments until they have been whitelisted.
+    #Prevents accidental exposure of sensitive info
+    #private method to allow us to pull info from the form require method ensure that a specific parameter is present
     #permit method returns a copy of the parameters object, returning only the permitted keys and values
-    #encapsulate artist_params in a private method so that it will only be available to thie particular class so that it won't work outside the scope of the controller
+    #encapsulate post_params in a private method so that it will only be available to thie particular class so that it won't work outside the scope of the controller
+    #also helpful b/c can reuse the same permit list with create and update
      private
      def post_params
        params.require(:post).permit(:author, :title, :body)
      end
 
-end #of class
+end
